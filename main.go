5b96bfa97ed0832edca854778cb60756bdfebd3f
github.com/DaveLampton <298615+DaveLampton@users.noreply.github.com>
2020-04-15 12:14:32 -0700

    verifying JSON data. deleting invalid files.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -26,23 +27,24 @@ var Formats = &formats{
 	CSV:  1,
 }
 
+// ChartLine represents a single song on the chart of a particular week.
+type ChartLine struct {
+	Rank     string
+	Song     string
+	Artist   string
+	LastWeek string
+	Trend    string
+	Movement string
+	Peak     string
+	Weeks    string
+}
+
 func main() {
 
 	err := parseArgs(os.Args)
 	if err != "" {
 		fmt.Println(err)
 		return
-	}
-
-	type ChartLine struct {
-		Rank     string
-		Song     string
-		Artist   string
-		LastWeek string
-		Trend    string
-		Movement string
-		Peak     string
-		Weeks    string
 	}
 
 	var thisWeek []ChartLine
@@ -80,28 +82,34 @@ func main() {
 
 		// open file for this week
 		log.Println("-- Week:", week.Format("2006-01-02"))
-		f, err := os.OpenFile("data/"+week.Format("2006")+"/"+week.Format("2006-01-02")+".json",
-			os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Println(err)
-		}
-		// instead of defer.close(), the file is explicitly closed below.
+		if _, err := os.Stat("data/" + week.Format("2006") + "/" + week.Format("2006-01-02") + ".json"); os.IsNotExist(err) {
+			fmt.Printf("File does not exist. Creating...\n")
 
-		c.Visit("https://www.billboard.com/charts/hot-100/" + week.Format("2006-01-02"))
-		if err != nil {
-			log.Println(string("Visit error: ") + err.Error())
-		}
+			f, err := os.OpenFile("data/"+week.Format("2006")+"/"+week.Format("2006-01-02")+".json",
+				os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Println(err)
+			}
+			// instead of defer.close(), the file is explicitly closed below.
 
-		var json = jsoniter.ConfigCompatibleWithStandardLibrary
-		jsonOut, _ := json.MarshalIndent(thisWeek, "", " ")
-		if _, err := f.Write(jsonOut); err != nil {
-			log.Println(err)
-		}
-		f.Close()
+			c.Visit("https://www.billboard.com/charts/hot-100/" + week.Format("2006-01-02"))
+			if err != nil {
+				log.Println(string("Visit error: ") + err.Error())
+			}
 
-		// throttle our requests
-		log.Println("self-throttle: pausing for two seconds")
-		time.Sleep(2 * time.Second)
+			var json = jsoniter.ConfigCompatibleWithStandardLibrary
+			jsonOut, _ := json.MarshalIndent(thisWeek, "", " ")
+			if _, err := f.Write(jsonOut); err != nil {
+				log.Println(err)
+			}
+			f.Close()
+
+			// throttle our requests
+			log.Println("self-throttle: pausing for three seconds")
+			time.Sleep(3 * time.Second)
+		} /*  else {
+			fmt.Println("Data file already exists.")
+		} */
 	}
 
 	log.Println("-- Done.")
@@ -111,7 +119,9 @@ func parseArgs(args []string) string {
 
 	var argCount = len(args) - 1
 	dataFormat := Formats.JSON // default format
-	verify := flag.Bool("verify", false, "verify that all JSON and CSV files in the data directory each contain 100 songs")
+	verify := flag.Bool("verify", false, "Verify that all JSON and CSV files"+
+		" in the data directory each contain 100 songs. Invalid"+
+		" data files will be deleted.")
 	CSV := flag.Bool("csv", false, "use the CSV data format")
 	flag.Parse()
 
@@ -131,6 +141,7 @@ func parseArgs(args []string) string {
 	if *verify {
 		argCount--
 		fmt.Println("Verify flag specified. startDate and endDate are ignored.")
+		fmt.Println("Checking all data... deleting invalid files.")
 		return verifyData()
 	}
 
@@ -143,22 +154,26 @@ func parseArgs(args []string) string {
 	// first week ever: released Aug 2, 1958, for the week of August 4th, 1958
 	now := time.Now()
 	firstWeek, _ := time.Parse("2006-01-02", "1958-08-04")
-	startDate, _ = time.Parse("2006-01-02", args[len(args)-2]) // command line argument one
+	startDate, _ = time.Parse("2006-01-02", args[len(args)-argCount]) //
 	if startDate.Before(firstWeek) {
 		startDate = firstWeek
 	}
+	fmt.Println(startDate.Format("2006-01-02"))
 
 	if argCount > 1 { // endDate was specified
-		endDate, _ = time.Parse("2006-01-02", args[len(args)-1]) // command line argument two
+		endDate, _ = time.Parse("2006-01-02", args[len(args)-1]) //
 	} else {
 		endDate = startDate.AddDate(0, 0, 140) // set default endDate to twenty weeks
 	}
+	//fmt.Println(endDate.Format("2006-01-02"))
+
 	if endDate.After(now) {
 		endDate = now
 	}
 	if endDate.Before(startDate) {
 		endDate = startDate
 	}
+	fmt.Println(endDate.Format("2006-01-02"))
 
 	// backup in time to the nearest Monday...
 	for startDate.Weekday(); startDate.Weekday() != 1; startDate = startDate.AddDate(0, 0, -1) {
@@ -181,5 +196,51 @@ func createDataDirectories(week time.Time) {
 }
 
 func verifyData() string {
-	return "Data checks out OK"
+	err := filepath.Walk("data",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			switch {
+			case err != nil:
+				log.Println("filepath.Walk: ", err)
+			case info.IsDir():
+				//fmt.Println(path, "Directory")
+			default:
+				//fmt.Println(path, info.Size())
+				f, err := os.Open(path)
+				if err != nil {
+					log.Println("OpenFile:", err)
+				}
+				var contents = make([]byte, info.Size())
+				_, err = f.Read(contents)
+				if err != nil {
+					log.Println("Reading:", err)
+				}
+				f.Close()
+				//fmt.Print("File contents:\n", string(contents))
+
+				theWeek := make([]ChartLine, 100)
+				err = jsoniter.Unmarshal(contents, &theWeek)
+				if err != nil {
+					log.Println("Unmarshalling:", err)
+				}
+				// for some rerason several charts in late 1976 to early 1977
+				// have only 99 songs in the Hot100, so I'll accept 99 songs
+				if len(theWeek) != 100 && len(theWeek) != 99 {
+					fmt.Println(path, ": len(theWeek):", len(theWeek))
+					// delete file
+					var err = os.Remove(path)
+					if err != nil {
+						fmt.Println("Problem deleteing file", path)
+					}
+					fmt.Println("File Deleted")
+				}
+			}
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+	return "Any invalid/incomplete files have been removed. Remaining data checks out OK."
 }
